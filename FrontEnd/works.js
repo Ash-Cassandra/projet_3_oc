e52e40b554a37db5000c86383177b2512bdc1abd
Ash-Cassandra <c.larcheveque@orange.fr>
2023-01-16 22:08:39 +0100

    affichages des trois boutons categories

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,52 +1,47 @@
+const responses = await fetch("http://localhost:5678/api/works");
+const works = await responses.json();
 
+const responsesCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await responsesCategories.json();
 
-const responses = await fetch ("http://localhost:5678/api/works");
-const works =  await responses.json ();
+document.querySelector(".gallery").innerHTML = "";
 
-document.querySelector(".gallery").innerHTML ='';
+/* affichage des projets sur le site */
 
-
-try {
-    responses;
-
-    if (responses.status === 200) {
-        genererProjects(works),
-        
-        console.log("ok");
-    } else {
-        throw new Error ('Unexpected Error');
-    }
-} catch (error) {
-    console.error(error);
-}
-
-
-function genererProjects(works){
-    for (let i = 0; i < works.length; i++) {
-        
-
-    const project = works [i];
+function genererProjects(works) {
+  for (let i = 0; i < works.length; i++) {
+    const project = works[i];
     const sectionGallery = document.querySelector(".gallery");
-      /* fiche projet */
+    /* fiche projet */
     const workElement = document.createElement("figure");
 
-  /* balises de la fiche */
+    /* balises de la fiche */
     const imageElement = document.createElement("img");
-        imageElement.src  = project.imageUrl;
-        imageElement.crossOrigin = "anonymous";
-        imageElement.alt = project.title;
+    imageElement.src = project.imageUrl;
+    imageElement.crossOrigin = "anonymous";
+    imageElement.alt = project.title;
 
     const titleElement = document.createElement("h2");
-        titleElement.innerText = project.title;
+    titleElement.innerText = project.title;
 
     sectionGallery.appendChild(workElement);
     workElement.appendChild(imageElement);
     workElement.appendChild(titleElement);
-    
-    }
+  }
 }
+genererProjects(works);
 
+/* affichage des boutons */
+const filters = document.querySelector("#filters");
 
+function genererButtons(categories) {
+  for (let i = 0; i < categories.length; i++) {
+    const filter = categories[i];
+    const button = document.createElement("button");
+    button.className = "filter";
+    button.innerText = filter.name;
 
-
-        
+    filters.appendChild(button);
+  }
+}
+genererButtons(categories);
